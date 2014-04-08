67feafd5508bf21f87abf45857f8130f161b3c3a
Jason Riordan <jriordan001@gmail.com>
2014-04-07 21:44:55 -0400

    fix fwpath derp

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,7 +90,7 @@ BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/prima_lan/parameters/fwpath"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/prima_wlan/parameters/fwpath"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
